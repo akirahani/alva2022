63b89af10f12f504972b56d737d318b8cdf8d8c2
akirahani <kuroshinichi2109@gmail.com>
2022-05-03 16:03:27 +0700

    'trucot+lavabo'

--- view/tru-cot-da/list.php
+++ view/tru-cot-da/list.php
@@ -6,22 +6,162 @@
     $mail = new PHPMailer(true);
 ?>
 <link rel="stylesheet" type="text/css" href="public/css/tru-cot.css?v=28042022">
-<section>
-    
-</section>
+<main class="tru-cot-da-all">
+    <section class="tru-cot-da">
+        <div class="bg-extand">
+            <h1>
+                Đá tự nhiên ốp cột trụ
+                <br>
+                sang trọng, hiện đại
+            </h1>
+        </div>
+    </section>
+    <article class="content-first">
+        <p>
+            Cột trụ nhà được ví như nốt nhạc hay nhất trong một bài nhạc. Cột nhà có vai trò cực kỳ quan trọng trong việc tô điểm thêm cho vẻ đẹp ngôi nhà. Chính vì vậy việc sử dụng đá ốp cột là rất cần thiết giúp tô điểm cho cột nhà thêm sang trọng hơn.
+        </p>
+    </article>
+    <ul class="img-list owl-carousel owl-theme">
+        <li><img src="view/tru-cot-da/upload/owl-tru-cot.png" alt="Trụ cột đá tự nhiên"></li>
+        <li><img src="view/tru-cot-da/upload/tru-cot1.2.png" alt="Trụ cột đá tự nhiên"></li>
+        <li><img src="view/tru-cot-da/upload/tru-cot1.3.png" alt="Trụ cột đá tự nhiên"></li>
+        <li><img src="view/tru-cot-da/upload/tru-cot1.4.png" alt="Trụ cột đá tự nhiên"></li>
+        <li><img src="view/tru-cot-da/upload/tru-cot1.5.png" alt="Trụ cột đá tự nhiên"></li>
+    </ul>
+    <section class="description1">
+        <article class="content-second">
+            <p>
+               không chỉ để nâng đỡ ngôi nhà mà còn đóng vai trò trang trí tuyệt vời. Đá tự nhiên làm nổi bật sự quý phái, sang trọng cho ngôi nhà.
+               Ngoài ra, hình dáng tròn mềm mại hay vuông vững chãi cũng tạo ra nhiều lựa chọn cho bạn.
+            </p>
+        </article>
+        <img src="view/tru-cot-da/upload/tru-cot-da2.png" alt="Trụ cột đá tự nhiên" class="img-2" />
+        <article class="content-third">
+            <p>
+                Cột đã tự nhiên có nhiều ưu điểm vượt trội so với vật liệu ốp cột thông thường.
+                <br><br>
+                Những công trình sử dụng đá tự nhiên thì bạn sẽ yên tâm hơn về độ bền của chúng.
+                <br><br>
+                Được sử dụng phải chống chịu được các điều kiện về thời tiết, sức nặng và các lực tác động.
+            </p>
+        </article>
+        <div class="group-tru-cot">
+            <h1>
+                được nhiều gia chủ lựa chọn 
+                <br>
+                đưa vào không gian sống
+            </h1>
+            <ul class="img-list-2 owl-carousel owl-theme">
+                <li><img src="view/tru-cot-da/upload/tru-cot2.1.png" alt="Trụ cột đá tự nhiên"></li>
+                <li><img src="view/tru-cot-da/upload/tru-cot2.2.png" alt="Trụ cột đá tự nhiên"></li>
+                <li><img src="view/tru-cot-da/upload/tru-cot2.3.png" alt="Trụ cột đá tự nhiên"></li>
+                <li><img src="view/tru-cot-da/upload/tru-cot2.4.png" alt="Trụ cột đá tự nhiên"></li>
+                <li><img src="view/tru-cot-da/upload/tru-cot2.5.png" alt="Trụ cột đá tự nhiên"></li>
+            </ul>
+            <p>
+                Trụ cột ốp lát đá tự nhiên thiết kế nổi tiếng từ xưa tới giờ, luôn được sử dụng khá phổ biến trong nhiều thiết kế nhà cổ điển hay hiện đại.
+                <br><br>
+                Để công trình đá ốp cột được toàn diện hơn bạn nên chọn được đơn vị thi công chất lượng, uy tín, chuyên nghiệp.
+            </p>
+        </div>
+    </section>
+    <section class="social">
+        <h1>Alva Stone</h1>
+        <p>Đưa tới sự khác biệt, khẳng định là đơn vị tư vấn, cung cấp và thi công lắp đặt các sản phẩm về đá tự nhiên cao cấp, ứng dụng công nghệ hiện đại và triết lý vượt qua thử thách để dẫn đầu.
+         Titan Stone quyết tâm trở thành thương hiệu uy tín tại Việt Nam.</p>
+        <div class="service">
+            <div class="detail-service">
+                <div class="icon">
+                    <img src="view/tru-cot-da/upload/icon1.png" alt="icon service">
+                </div>
+                <p>
+                    Miễn phí 
+                    <br>
+                    Tư vấn, thiết kế
+                </p>
+            </div>
+            <div class="detail-service">
+                <div class="icon">
+                    <img src="view/tru-cot-da/upload/icon2.png" alt="icon service">
+                </div>
+                 <p>
+                    Lắp đặt
+                    <br>
+                    hoàn thiện 2 ngày
+                </p>
+            </div>
+            <div class="detail-service">
+                <div class="icon">
+                    <img src="view/tru-cot-da/upload/icon3.png" alt="icon service">
+                </div>
+                <p>
+                    Bảo hành Đá
+                    <br>
+                    01 năm
+                </p>
+            </div>
+            <div class="detail-service">
+                <div class="icon">
+                    <img src="view/tru-cot-da/upload/icon4.png" alt="icon service">
+                </div>
+                <p>
+                    Bảo hành đèn led
+                    Samsung 05 năm
+                </p>
+            </div>
+        </div>
+        <ul class="company-info">
+            <li><img src="view/tru-cot-da/upload/place.svg" alt="Showroom" /> <p>Showroom: Số 2 Mạc Đăng Doanh, Dương Kinh, Hải Phòng</p></li>
+            <li><img src="view/tru-cot-da/upload/tel.svg" alt="Hotline" /><p>Hotline: 1900 669 996</p></li>
+            <li><img src="view/tru-cot-da/upload/fb.svg" alt="Fanpage" /><p>Fanpage: Đá Tự Nhiên Titan Stone</p></li>
+            <li><img src="view/tru-cot-da/upload/mail.svg" alt="Email" /><p>Email: Titanstone@gmail.com</p></li>
+        </ul>
+    </section>
+    <section class="contact">
+        <h1>
+            Gửi thông tin của bạn cho chúng tôi 
+            <br>
+            Để được tư vấn miễn phí tốt nhất
+        </h1>
+        <div class="form-contact-box" id="form-contact">
+            <form class="form-area" method="post">
+                <div class="input-placeholder">
+                    <input type="text" name="name" required spellcheck="false" autocomplete="off">
+                    <div class="placeholder-ct">
+                        Tên của bạn (<span>*</span>)
+                    </div>
+                </div>
+                <div class="input-placeholder">
+                    <input type="text" name="email" required spellcheck="false" autocomplete="off">
+                    <div class="placeholder-ct">
+                        Email (<span>*</span>)
+                    </div>
+                </div>
+                <div class="input-placeholder">
+                    <input type="text" name="phone" required spellcheck="false" autocomplete="off">
+                    <div class="placeholder-ct">
+                        Số điện thoại (<span>*</span>)
+                    </div>
+                </div>
+                <textarea type="text" name="note" placeholder="Tin nhắn"></textarea>
+                <button type="sumbit" name="submit">Gửi liên hệ</button>
+            </form>            
+        </div>
+    </section>
+</main>
 <script type="text/javascript">
     $('.img-list').owlCarousel({
         loop:true,
         margin:10,
-        nav:false,
+        nav:true,
         autoplay: false,
         autoplayTimeout:5000,
-        items: 1
+        items: 1,
     })
 
     $('.img-list-2').owlCarousel({
         loop:true,
-        margin:10,
+        margin: -70,
         nav:true,
         autoplay: true,
         autoplayTimeout:5000,
