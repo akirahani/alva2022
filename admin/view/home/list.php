1049aed98735e0bc2ae6e3bdccaea890def1d4c0
akirahani <kuroshinichi2109@gmail.com>
2022-05-13 17:26:44 +0700

    'ok'

--- admin/view/home/list.php
+++ admin/view/home/list.php
@@ -11,6 +11,10 @@
     $title2 = round( ((count($dm2)/count($data_da))*100),2 );
     $title3 = round( ((count($dm3)/count($data_da))*100),2 );
     $title4 = round( ((count($dm4)/count($data_da))*100),2 );
+    // Đơn hàng
+    $query->DanhSach('donhang',['id','trangthai','ngay','lydohuy'],['trangthai'=>'='],[],[],['trangthai'=>1]);
+    $query->DanhSach('donhang',['id','trangthai','ngay','lydohuy'],['trangthai'=>'='],[],[],['trangthai'=>3]);
+    $query->DanhSach('donhang',['id','trangthai','ngay','lydohuy'],['trangthai'=>'='],[],[],['trangthai'=>4]);
 ?>
 <div class="thongke-baocao">
     <div class="chart2">
@@ -18,15 +22,13 @@
         <h1 class="title-thongke">Thống kê sản phẩm</h1>
     </div>
     <div class="chart1">
-        <canvas id="pieChart1" title1="<?= $title1?>" title2="<?= $title2?>" title3="<?= $title3?>" title4="<?= $title4?>" sl1="<?=count($dm1)?>" sl2="<?=count($dm2)?>" sl3="<?=count($dm3)?>" sl4="<?=count($dm4)?>"></canvas>
+        <canvas id="barChart" title1="<?= $title1?>" title2="<?= $title2?>" title3="<?= $title3?>" title4="<?= $title4?>" sl1="<?=count($dm1)?>" sl2="<?=count($dm2)?>" sl3="<?=count($dm3)?>" sl4="<?=count($dm4)?>"></canvas>
         <h1 class="title-thongke">Thống kê đơn hàng</h1>
     </div>
 </div>
 <script type="text/javascript" src="lib/chart/utils.js"></script>
 <script>
-// const ctx = $('#myChart').getContext('2d');
 const ctx = $("#pieChart")[0].getContext('2d');
-const ctx1 = $("#pieChart1")[0].getContext('2d');
 
 var sltranhda = $('#pieChart').attr('sl1');
 var sloplat = $('#pieChart').attr('sl2');
@@ -50,14 +52,6 @@ const pieChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
             borderWidth: 1
         }]
     },
@@ -79,52 +73,53 @@ const pieChart = new Chart(ctx, {
 // var tloplat1 = $('#pieChart1').attr('title2');
 // var tlcotda1 = $('#pieChart1').attr('title3');
 // var tllavabo1 = $('#pieChart1').attr('title4');
-// 
-// const DATA_COUNT = 7;
-// const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
 
-// const labels = Utils.months({count: 7});
-// const data = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: Utils.numbers(NUMBER_CFG),
-//       backgroundColor: Utils.CHART_COLORS.red,
-//     },
-//     {
-//       label: 'Dataset 2',
-//       data: Utils.numbers(NUMBER_CFG),
-//       backgroundColor: Utils.CHART_COLORS.blue,
-//     },
-//     {
-//       label: 'Dataset 3',
-//       data: Utils.numbers(NUMBER_CFG),
-//       backgroundColor: Utils.CHART_COLORS.green,
-//     },
-//   ]
-// };
-
-// const config = new Chart(ctx1, {
-//     type: 'bar',
-//     data: data,
-//     options: {
-//     plugins: {
-//     title: {
-//         display: true,
-//         text: 'Chart.js Bar Chart - Stacked'
-//       },
-//     },
-//     responsive: true,
-//     scales: {
-//       x: {
-//         stacked: true,
-//       },
-//       y: {
-//         stacked: true
-//       }
-//     }
-//   }
-// });
-
+const ctx1 = $("#barChart")[0].getContext('2d');
+const myChart1 = new Chart(ctx1, {
+    type: 'bar',
+    data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'
+        ],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 </script>
